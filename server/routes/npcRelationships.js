9b89235259aeeeebf7f9fbe091a749f706d9ab0d
Rabie Jacobs <rabiej30@gmail.com>
2025-03-14 17:10:26 +0200

    Test staging

--- server/routes/npcRelationships.js
+++ server/routes/npcRelationships.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db');
-
+  
 // Fetch all NPC relationships for a user
 router.get('/:userId', async (req, res) => {
   const { userId } = req.params;
